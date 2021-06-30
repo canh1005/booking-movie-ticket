c70422b28bf65f7364634bc046057145d4186579
CảnhNguyễn <nguyenhuucanh1505@gmail.com>
2021-06-30 18:41:06 +0700

    design navbar v2

--- src/components/HomeComponents/homeNavbar.js
+++ src/components/HomeComponents/homeNavbar.js
@@ -1,44 +1,42 @@
 import React, { useState } from 'react'
-// import { NavLink } from 'react-router-dom'
+import { NavLink, withRouter } from 'react-router-dom';
 import T_logo from "../../assets/images/T_logo.png"
 
-export default function HomeNavbar() {
+function HomeNavbar() {
     const [isOpen, setOpen] = useState("false")
-
     return (
         <div>
             <header className="flex justify-between bg-green-500 px-5 pt-3 items-center text-white">
-
                 {/* Logo */}
                 <div className="flex flex-1 space-x-3 items-center">
                     {/* icon */}
                     <div className="w-20 h-20" >
-                        <img className="" src={T_logo}></img>
+                        <img className="" src={T_logo} alt=""></img>
                     </div>
                     {/* Name-branch */}
-                    <div className="text-2xl">
+                    <div className="text-2xl lg:hidden">
                         <p> ABCXYZ-cinema </p>
                     </div>
                 </div>
-                {/* Menu */}
-                <div className=" space-x-3 text-xl xs:hidden md:block ">
-                    {!isOpen ? (                    <div>
-                        <a className=" transition duration-400  active:bg-gray-200  p-2 rounded-md hover:text-green-500 hover:bg-white" href="a">Home</a>
-                        <a className="transition duration-400  active:bg-gray-200  p-2 rounded-md hover:text-green-500 hover:bg-white" href="b">Contact</a>
-                    </div>) : <span></span>}
-
-
+                {/* Menu on destop */}
+                <div className="space-x-3 text-base md:text-lg">
+                    <NavLink activeClassName="underline" className=" transition duration-400 active:bg-gray-200  p-2 rounded-md hover:text-green-500 hover:bg-white" to="a">Home</NavLink>
+                    <NavLink activeClassName="underline" className="transition duration-400 active:bg-gray-200  p-2 rounded-md hover:text-green-500 hover:bg-white" to="b">Contact</NavLink>
                 </div>
                 {/* Menu_icon */}
-                <div className="md:hidden cursor-pointer ">
-                    <button onClick={() => setOpen(!isOpen)} type="button">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                    </button>
-
-                </div>
+                <button className="cursor-pointer md:hidden" onClick={() => setOpen(!isOpen)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
             </header>
+            {/* Menu on mobile */}
+            <div className={!isOpen ? "flex flex-col text-white text-center p-5 bg-green-500" : "hidden"}>
+                <NavLink activeClassName="underline" className="transition duration-400 p-2 rounded-md hover:underline" to="a">Home</NavLink>
+                <NavLink activeClassName="underline" className="transition duration-400 p-2 rounded-md hover:underline" to="b">Contact</NavLink>
+            </div>
         </div>
     )
 }
+const completeHomeNavbar = HomeNavbar;
+export default withRouter(completeHomeNavbar);
