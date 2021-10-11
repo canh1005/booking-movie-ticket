53ce8647674ecd6e4e64d9ed15fff476c9850075
megastar35 <lieudungtri@gmail.com>
2021-10-11 23:28:42 +0700

    Cinema_logo
    Adding CinemaLogo component

--- src/components/HomeComponents/MovieDetail/Calendar.js
+++ src/components/HomeComponents/MovieDetail/Calendar.js
@@ -1,13 +1,37 @@
+import moment from 'moment';
 import React from 'react'
 
 export default function Calendar() {
+    const getCurrentTimes = () => {
+        const weekdays = [];
+        let now = moment();
+        let currentDay = now.startOf("isoWeek");
+        for (let index = 0; index <= 6; index++) {
+            weekdays.push(moment(currentDay).add(index, 'days').locale("vi").format("DD/MM")) // thêm thứ ngày tháng năm của tuần hiện tại , iso 8601
+        }
+        return weekdays.map((day, index) => {
+
+            return <li className="mr-2 border-2 rounded-md p-2 hover:bg-white hover:text-black"><a href="#acb">
+                <span>{index === 6 ? "Chủ nhật" : `Thứ ${index + 2}`}</span>
+                <br />
+                <span>
+                    {day}
+                </span>
+            </a>
+
+            </li>
+
+        }
+
+        )
+    }
+
     return (
         <div>
             <h2 className="text-center">Lịch</h2>
-            <ul className="flex justify-center items-center">
-                <li className="mr-2 border-2 rounded-md p-2 hover:bg-white hover:text-black"><a href="#acb">abc</a></li>
-                <li className="mr-2 border-2 rounded-md p-2 hover:bg-white hover:text-black"><a href="#acb">bbc</a></li>
-                <li className="mr-2 border-2 rounded-md p-2 hover:bg-white hover:text-black"><a href="#acb">cbc</a></li>
+            <ul className="flex text-center justify-center items-center">
+                {getCurrentTimes()}
+
             </ul>
         </div>
     )
